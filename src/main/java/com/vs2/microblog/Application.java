d13258cd6306ddc3b30290a2c869ae04b6d3b62b
Waldemar Schlegel <walde1024@gmail.com>
2016-03-21 09:56:25 +0100

    Initial commit

---
+++ src/main/java/com/vs2/microblog/Application.java
@@ -0,0 +1,16 @@
+package com.vs2.microblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.applet.resources.MsgAppletViewer_ko;
+
+/**
+ * Created by Walde on 20.03.16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/vs2/microblog/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.vs2.microblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Walde on 19.03.16.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value="name", defaultValue="World", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "home";
+    }
+}
