1bf302684936c9c3da273f1b2f657d479281ba23
Waldemar Schlegel <walde1024@gmail.com>
2016-03-26 14:04:37 +0100

    Added redis dependencies and config

--- src/main/java/com/vs2/microblog/Application.java
+++ src/main/java/com/vs2/microblog/Application.java
@@ -2,6 +2,10 @@ package com.vs2.microblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import sun.applet.resources.MsgAppletViewer_ko;
 
 /**
@@ -13,4 +17,22 @@ public class Application {
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean(name="jedisConnFactory")
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setUsePool(true);
+
+        return factory;
+    }
+
+    @Bean(name="redisTemplateString")
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<String, String>();
+        template.setConnectionFactory(jedisConnectionFactory());
+
+        return template;
+    }
+
+
 }
